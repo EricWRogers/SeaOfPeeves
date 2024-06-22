13b70b2adf2c7700eab3625e33c0203a89cd78da
Blake-Henderson <bmhenderson101@gmail.com>
2024-06-22 10:45:33 -0500

    Boat goes

--- Assets/Scripts/BoatControls.cs
+++ Assets/Scripts/BoatControls.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Permissions;
 using TMPro;
 using UnityEngine;
 
@@ -10,15 +11,20 @@ public class BoatControls : MonoBehaviour
     public TMP_Text speed;
     public float acceleration = 10f;
     public float decceleration = -5f;
+    public float turnRotation = 15f;
     public float turnSpeed = 15f;
     public float maxSpeed = 25f;
     public float anchorDrag = 10f;
+    public float straightenTime = 1f;
     private float baseDrag;
 
     private Rigidbody rb;
     private Vector3 direction = Vector3.forward;
     private float currentAcceleration = 0f;
     private bool anchored = false;
+    private bool goingLeft = false;
+    private bool goingRight = false;
+    private float straightenTimer = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -30,22 +36,42 @@ public class BoatControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(!anchored && goingLeft || goingRight)
+        {
+            straightenTimer += Time.deltaTime;
+            if (straightenTimer > straightenTime)
+            {
+                straight();
+            }
+        }        
     }
 
     private void FixedUpdate()
     {
-        if ( !anchored && !(rb.velocity.sqrMagnitude >= maxSpeed && currentAcceleration > 0) && !(rb.velocity.sqrMagnitude <= -maxSpeed && currentAcceleration < 0))
-        {
-            rb.AddForce(direction * currentAcceleration * Time.deltaTime);
-        }
-        else if(anchored)
+        if (anchored)
         {
             rb.drag = anchorDrag;
         }
         else if (!anchored)
         {
-            rb.drag = 0;
+            rb.drag = baseDrag;
+        }
+
+        if (goingLeft)
+        {
+            //direction += new Vector3(0, -turnRotation, 0).normalized * turnSpeed * Time.deltaTime;
+            Boat.transform.Rotate(Vector3.up, -turnRotation * turnSpeed * Time.deltaTime);
+        }
+        else if (goingRight)
+        {
+            Debug.Log("going right");
+            //direction += new Vector3(0, turnRotation, 0).normalized * turnSpeed * Time.deltaTime;
+            Boat.transform.Rotate(Vector3.up, turnRotation * turnSpeed * Time.deltaTime);
+        }
+
+        if ( !anchored && !(rb.velocity.sqrMagnitude >= maxSpeed && currentAcceleration > 0) && !(rb.velocity.sqrMagnitude <= -maxSpeed && currentAcceleration < 0))
+        {
+            rb.AddForce(Boat.transform.forward * currentAcceleration * Time.deltaTime);
         }
     }
 
@@ -62,12 +88,25 @@ public class BoatControls : MonoBehaviour
 
     public void left()
     {
-        
+        straightenTimer = 0f;
+        goingLeft = true;
+        goingRight = false;
+        Debug.Log("left");
     }
 
     public void right()
     {
+        straightenTimer = 0f;
+        goingRight = true;
+        goingLeft = false;
+        Debug.Log("right");
+    }
 
+    public void straight()
+    {
+        straightenTimer = 0f;
+        goingLeft = false;
+        goingRight = false;
     }
 
     public void anchor()
