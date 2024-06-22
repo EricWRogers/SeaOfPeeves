6e8e74f58238e21fec8c143f29f025f36f35cae9
Ben <mynameizkevin@gmail.com>
2024-06-22 09:19:34 -0400

    health bar

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,14 +2,17 @@ using SuperPupSystems.Helper;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public Health health;
 
+    public Slider healthSlider;
+
     void Start()
     {
-
+        health.Revive();
     }
 
     
@@ -20,21 +23,23 @@ public class PlayerHealth : MonoBehaviour
 
     public void OnHealthChanged()
     {
-        // other on healing things
+        Debug.Log("Updating health:" +  (float)health.currentHealth / (float)health.maxHealth);
+        healthSlider.value = (float)health.currentHealth / (float)health.maxHealth;
     }
 
     public void Healed()
     {
-        // Play a healing vfx
+        
     }
 
     public void Hurt()
     {
-        // Play a hurt vfx/sounds
+        
     }
 
     public void OutOfHealth()
     {
         // Respawn and reset things here
+        health.Revive();
     }
 }
