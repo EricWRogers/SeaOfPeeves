5642c12c236b0c163c284e9df837e920f9953ab2
Blake-Henderson <bmhenderson101@gmail.com>
2024-06-22 22:54:08 -0500

    Harpoon :)

--- Assets/Scripts/HarpoonGun.cs
+++ Assets/Scripts/HarpoonGun.cs
@@ -5,9 +5,12 @@ using UnityEngine.InputSystem;
 
 public class HarpoonGun : MonoBehaviour
 {
-    GameObject harpoon;
+    public GameObject harpoon;
+    public Transform firePoint;
+    private GameObject shot;
     private PlayerInputActions playerControls;
     private InputAction fire;
+    public bool fired;
     // Start is called before the first frame update
     private void Awake()
     {
@@ -24,14 +27,17 @@ public class HarpoonGun : MonoBehaviour
         fire.Disable();
     }
 
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if(!fired && playerControls.Player.Fire.triggered) { 
+            fired = true;
+            shot = GameObject.Instantiate(harpoon, firePoint.position, transform.rotation);
+            shot.GetComponent<Harpoon>().returnPoint = firePoint;
+            shot.GetComponent<Harpoon>().ret = false;
+        }
+        else if(fired && playerControls.Player.Fire.triggered) {
+            shot.GetComponent<Harpoon>().ret = true;
+        }
     }
 }
